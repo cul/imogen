af4ce05c1272fb0c1f89d6f14b8da24a72a9bea6
Benjamin Armintor <armintor@gmail.com>
2019-10-10 18:05:53 -0400

    WIP extconf lib locations for opencv4

--- ext/imogencv/extconf.rb
+++ ext/imogencv/extconf.rb
@@ -7,6 +7,13 @@ end
 
 append_cflags('-stdlib=libc++')
 
+incdir, libdir = dir_config("opencv4", "/usr/local/include", "/usr/local/lib")
+
+unless have_header('opencv2/features2d.hpp') do |pt| puts "tried header at #{pt}"; pt; end
+	print("need opencv4\n")
+	exit 1
+end
+
 puts CONFIG["configure_args"]
 $LIBRUBYARG.prepend(' ') # there's some weird spacing issue in rice's lib linking routine
 create_makefile('imogencv')

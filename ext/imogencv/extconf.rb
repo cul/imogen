6d3b60b1fccf54f20ac76ef05e1bd6153adb7636
Benjamin Armintor <armintor@gmail.com>
2019-10-17 14:02:12 -0400

    whatever Rice does

--- ext/imogencv/extconf.rb
+++ ext/imogencv/extconf.rb
@@ -1,19 +1,11 @@
 require 'mkmf-rice'
-pkg_config('opencv4')
+
 osx = RbConfig::CONFIG['target_os'] =~ /darwin/
 if osx
 	$CFLAGS << " " << '-std=c++14' # damn the torpedoes!
 end
-
 append_cflags('-stdlib=libc++')
-
+@libdir_basename ||= 'lib'
 incdir, libdir = dir_config("opencv4", "/usr/local/include", "/usr/local/lib")
-
-unless have_header('opencv2/features2d.hpp') do |pt| puts "tried header at #{pt}"; pt; end
-	print("need opencv4\n")
-	exit 1
-end
-
-puts CONFIG["configure_args"]
 $LIBRUBYARG.prepend(' ') # there's some weird spacing issue in rice's lib linking routine
 create_makefile('imogencv')

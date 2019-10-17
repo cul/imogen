11cf1099baa8ad263dc81b1a5806294a813225e1
Benjamin Armintor <armintor@gmail.com>
2019-10-17 14:46:14 -0400

    use packaged opencv4

--- ext/imogencv/extconf.rb
+++ ext/imogencv/extconf.rb
@@ -3,9 +3,11 @@ require 'mkmf-rice'
 osx = RbConfig::CONFIG['target_os'] =~ /darwin/
 if osx
 	$CFLAGS << " " << '-std=c++14' # damn the torpedoes!
+	incdir, libdir = dir_config("opencv4", "/usr/local/include", "/usr/local/lib")
+else
+	incdir, libdir = dir_config("opencv", "/usr/local/include", "/usr/local/lib")
 end
 append_cflags('-stdlib=libc++')
 @libdir_basename ||= 'lib'
-incdir, libdir = dir_config("opencv4", "/usr/local/include", "/usr/local/lib")
 $LIBRUBYARG.prepend(' ') # there's some weird spacing issue in rice's lib linking routine
 create_makefile('imogencv')

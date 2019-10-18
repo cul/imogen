c8719e5c2edca189e3dfdee76d02b801ce0ff99c
Benjamin Armintor <armintor@gmail.com>
2019-10-18 11:57:16 -0400

    try opencv4 and opencv configs

--- ext/imogencv/extconf.rb
+++ ext/imogencv/extconf.rb
@@ -10,20 +10,27 @@ def real_inc_dir(src)
 	 File.symlink?(src) ? File.realdirpath(src) : src
 end
 
-incdir, libdir = dir_config("opencv", '/usr/local/include/opencv4', '/usr/local/lib/opencv4')
-
-unless find_header('opencv2/features2d.hpp', incdir)
-	puts "find_header failed (opencv4)"
-	a_file = File.join(incdir, 'opencv2/features2d.hpp')
+def add_flags_if_header(header, header_dir, lib_dir)
+	a_file = File.join(header_dir, header)
 	exists = File.exist?(a_file)
 	puts "#{a_file} exists ... #{exists}"
 	if exists
-		inc_opt = "-I#{incdir}".quote
-		lib_opt = "-L#{libdir}".quote
+		inc_opt = "-I#{header_dir}".quote
+		lib_opt = "-L#{lib_dir}".quote
 		puts "adding compiler flags:\n#{inc_opt}\n#{lib_opt}"
 		$INCFLAGS << " " << inc_opt
-		$LIBPATH = $LIBPATH | [libdir]
-	else
+		$LIBPATH = $LIBPATH | [lib_dir]
+	end
+	exists
+end
+
+incdir4, libdir4 = dir_config("opencv4", '/usr/local/include/opencv4', '/usr/local/lib/opencv4')
+incdir, libdir = dir_config("opencv", '/usr/local/include/opencv', '/usr/local/lib/opencv')
+
+opencv_header = 'opencv2/features2d.hpp'
+unless find_header(opencv_header, incdir) || find_header(opencv_header, incdir4)
+	puts "find_header failed (opencv4)"
+	unless add_flags_if_header(opencv_header, incdir4, libdir4) || add_flags_if_header(opencv_header, incdir, libdir)
 		exit 1
 	end
 end

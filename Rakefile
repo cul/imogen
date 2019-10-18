f52feb3b57efca18d129cfa1932df1307f1976d1
Benjamin Armintor <armintor@gmail.com>
2019-10-18 17:38:28 -0400

    defer to user args

--- Rakefile
+++ Rakefile
@@ -3,5 +3,10 @@
 require 'rubygems'
 require 'rake/extensiontask'
 
-Rake::ExtensionTask.new('imogencv')
+Rake::ExtensionTask.new('imogencv') do |ext|
+	ix = ARGV.index('opencv4-include')
+	ext.config_options << "--with-opencv4-include=#{ARGV[ix + 1]}" if ix
+	ix = ARGV.index('opencv4-lib')
+	ext.config_options << "--with-opencv4-lib=#{ARGV[ix + 1]}" if ix
+end
 Rake::Task[:spec].prerequisites << :compile
--- ext/imogencv/extconf.rb
+++ ext/imogencv/extconf.rb
@@ -24,19 +24,29 @@ def add_flags_if_header(header, header_dir, lib_dir)
 	exists
 end
 
-incdir4, libdir4 = dir_config("opencv4", '/usr/local/include/opencv4', '/usr/local/lib/opencv4')
-incdir, libdir = dir_config("opencv", '/usr/local/include/opencv', '/usr/local/lib/opencv')
+incdir_default = "/usr/local/include"
+libdir_default = "/usr/local/lib"
+
+# expect to call with --with-opencv4-include=DIR and --with-opencv4-lib=DIR
+incdir, libdir = dir_config("opencv4", incdir_default, libdir_default)
+unless incdir && incdir != incdir_default
+	puts "using default opencv4 include path: #{incdir}"
+end
 
 opencv_header = 'opencv2/features2d.hpp'
-unless find_header(opencv_header, incdir) || find_header(opencv_header, incdir4)
-	puts "find_header failed (opencv4)"
-	unless add_flags_if_header(opencv_header, incdir4, libdir4) ||
-		add_flags_if_header(opencv_header, incdir, libdir) ||
-		add_flags_if_header(opencv_header, '/usr/local/include', '/usr/local/lib') ||
-		add_flags_if_header(opencv_header, '/usr/include', '/usr/lib')
+
+unless find_header(opencv_header, *[incdir, incdir_default, "/usr/local"].compact.uniq)
+	header_path = File.join(incdir, opencv_header)
+	exist = File.exist?(header_path)
+	puts "header exist at #{header_path} : #{exist}"
+	tried = try_header(cpp_include(opencv_header), "-I#{incdir}".quote)
+
+	unless tried and add_flags_if_header(opencv_header, incdir, libdir)
+		puts "Cannot find required header: #{opencv_header}"
 		exit 1
 	end
 end
+
 append_cflags('-stdlib=libc++')
 @libdir_basename ||= 'lib'
 $LIBRUBYARG.prepend(' ') # there's some weird spacing issue in rice's lib linking routine

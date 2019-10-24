9eb801080321a8e70774fac8d9c304421c228934
Benjamin Armintor <armintor@gmail.com>
2019-10-24 15:47:28 -0400

    explicitly check opencv dependencies in case they are not linked

--- ext/imogencv/extconf.rb
+++ ext/imogencv/extconf.rb
@@ -32,48 +32,63 @@ libdir_default = "/usr/local/lib"
 have_library('stdc++')
 # MakeMakefile::CONFTEST_C = "#{CONFTEST}.cc"
 
-# with --with-opencv4-config=FILEPATH
-opencv4_config = (pkg_config('opencv4') || []).detect { |c| c =~ /\-L\/\w+/ }
+required_headers = {}
+required_libs = {}
 
-# expect to call with --with-opencv4-include=DIR and --with-opencv4-lib=DIR or --withopencv4-dir=DIR
-incdir, libdir = dir_config("opencv4", incdir_default, libdir_default) unless opencv4_config
-
-unless !opencv4_config && incdir && incdir != incdir_default
-	puts "using default opencv4 include path: #{incdir_default}"
-end
-
-unless !opencv4_config && libdir && libdir != libdir_default
-	puts "using default opencv4 library path: #{incdir_default}"
-end
-
-include_paths = [incdir_default, "/usr/local"]
-include_paths = ([incdir, File.join(incdir, "opencv4")] | include_paths) if incdir
-
-opencv_header = 'opencv2/features2d.hpp'
-
-unless find_header(opencv_header, *include_paths.compact.uniq)
-	open(MakeMakefile::Logging.instance_variable_get(:@logfile), 'r') do |logblob|
-		logblob.each { |logline| puts logline.strip }
-	end
-	puts "Cannot find required header: #{opencv_header}"
-	puts "if this output is from rake compile, consider adding:"
-	puts "rake compile -- --with-opencv4-include=DIR"
-	exit 1
-end
-
-lib_paths = [libdir_default, "/usr/local"]
-lib_paths.unshift(libdir) if libdir
-
-required_libs = [
+required_headers['opencv4'] = [ 'opencv2/features2d.hpp' ]
+required_libs['opencv4'] = [
 	'opencv_core',
 	'opencv_imgcodecs',
 	'opencv_imgproc',
 	'opencv_features2d'
 ]
-required_libs.each do |lib|
-	unless find_library(lib, nil, *lib_paths.compact.uniq)
-		puts "Cannot find required lib: #{lib}"
-		exit 1
+
+required_libs['zlib'] = ['z']
+required_libs['libwebp'] = ['webp']
+required_libs['libjpeg'] = ['jpeg']
+required_libs['libtiff-4'] = ['tiff']
+required_libs['libpng'] = ['png16']
+required_libs['jasper'] = ['jasper']
+required_libs['OpenEXR'] = ['IlmImf']
+
+all_deps = (required_libs.keys | required_headers.keys).sort.uniq
+all_deps.each do |dep_key|
+	has_pkg_config = (pkg_config(dep_key) || []).detect { |c| c =~ /\-L\/\w+/ }
+
+	# expect to call with --with-opencv4-include=DIR and --with-opencv4-lib=DIR or --withopencv4-dir=DIR
+	incdir, libdir = dir_config(dep_key, incdir_default, libdir_default) unless has_pkg_config
+
+	unless !has_pkg_config && incdir && incdir != incdir_default
+		puts "using default #{dep_key} include path: #{incdir_default}"
+	end
+
+	unless !has_pkg_config && libdir && libdir != libdir_default
+		puts "using default #{dep_key} library path: #{incdir_default}"
+	end
+
+	include_paths = [incdir_default, "/usr/local"]
+	include_paths = ([incdir, File.join(incdir, dep_key)] | include_paths) if incdir
+
+	required_headers.fetch(dep_key, []).each do |hdr|
+		unless find_header(hdr, *include_paths.compact.uniq)
+			open(MakeMakefile::Logging.instance_variable_get(:@logfile), 'r') do |logblob|
+				logblob.each { |logline| puts logline.strip }
+			end
+			puts "Cannot find required header: #{hdr}"
+			puts "if this output is from rake compile, consider adding:"
+			puts "rake compile -- --with#{dep_key}-include=DIR"
+			exit 1
+		end
+	end
+
+	lib_paths = [libdir_default, "/usr/local"]
+	lib_paths.unshift(libdir) if libdir
+
+	required_libs.fetch(dep_key, []).each do |lib|
+		unless find_library(lib, nil, *lib_paths.compact.uniq)
+			puts "Cannot find required lib: #{lib}"
+			exit 1
+		end
 	end
 end
 

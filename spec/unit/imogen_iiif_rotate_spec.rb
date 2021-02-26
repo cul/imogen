d3b385dd9fffc01ff2a5d6b6083a04a7c9a56c78
Benjamin Armintor <armintor@gmail.com>
2021-02-26 17:30:43 -0500

    unit test Imogen::Iiif::Rotation.convert - added in review for DERIVATIVO-24

--- spec/unit/imogen_iiif_rotate_spec.rb
+++ spec/unit/imogen_iiif_rotate_spec.rb
@@ -1,11 +1,9 @@
 require 'imogen/iiif'
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Imogen::Iiif::Rotation, type: :unit do
-  before(:all) do
-    @test_image = ImageStub.new(175,131)
-  end
-  subject {Imogen::Iiif::Rotation.new(@test_image)}
+  let(:image) { double("image", width: 175, height: 131) }
   describe "#get" do
+    subject { Imogen::Iiif::Rotation.new(image) }
     describe "with values mod 360 in 90 degree rotations" do
       it "should return [0, false] angle and flip for 0 or '0' or nil" do
         expect(subject.get(0)).to eq([0, false])
@@ -47,6 +45,33 @@ describe Imogen::Iiif::Rotation, type: :unit do
     end
     it "should reject bad values" do
       expect{subject.get("-2,")}.to raise_error Imogen::Iiif::BadRequest
+    end
+  end
+  describe '.convert' do
+    let(:no_op) { Proc.new {|x| x} }
+    context 'at multiple of 360' do
+      it "does not rotate" do
+        expect(image).not_to receive(:rot)
+        expect(image).not_to receive(:fliphor)
+        (-2..2).each { |x| described_class.convert(image, (x*360).to_s, &no_op) }
+      end
+    end
+    context 'at right angle rotations not multiple of 360' do
+      it "does rotate" do
+        expect(image).not_to receive(:fliphor)
+        [-3, -2, -1, 1, 2, 3].each do |x|
+          tuple = Imogen::Iiif::Rotation.new(image).get((90*x).to_s)
+          param = "d#{tuple[0]}"
+          expect(image).to receive(:rot).with(param)
+          described_class.convert(image, (x*90).to_s, &no_op)
+        end
+      end
+    end
+    context 'with a bang param' do
+      it "flips horizontal" do
+        expect(image).to receive(:fliphor)
+        described_class.convert(image, "!0", &no_op)
+      end
     end
   end
 end

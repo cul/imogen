db550ec4bf219cace836dbe11fef49232c36d32c
Eric O <eric@ejdigitalmedia.com>
2023-11-07 15:11:37 -0500

    Fixes for iiif tile generation
    - Previous implementation was not generating correct set of tiles for the OpenSeadragon viewer (so relying on cached-only tiles wasn't working).
    - Also changed File.exists? to File.exist? to support newer versions of Ruby.

---
+++ spec/integration/imogen_iiif_tiles_spec.rb
@@ -0,0 +1,93 @@
+require 'imogen'
+require 'tmpdir'
+
+describe Imogen::Iiif::Tiles, vips: true do
+  let(:tile_size) { 512 }
+  let(:output_dir) { Dir.tmpdir + '/tile-output-dir' }
+  let(:expected_files) do
+    [
+      '/0,0,512,512/512,/0/default.jpg',
+      '/0,512,512,512/512,/0/default.jpg',
+      '/0,1024,512,512/512,/0/default.jpg',
+      '/0,1536,512,512/512,/0/default.jpg',
+      '/0,2048,512,512/512,/0/default.jpg',
+      '/0,2560,512,512/512,/0/default.jpg',
+      '/0,3072,512,53/512,/0/default.jpg',
+      '/512,0,512,512/512,/0/default.jpg',
+      '/512,512,512,512/512,/0/default.jpg',
+      '/512,1024,512,512/512,/0/default.jpg',
+      '/512,1536,512,512/512,/0/default.jpg',
+      '/512,2048,512,512/512,/0/default.jpg',
+      '/512,2560,512,512/512,/0/default.jpg',
+      '/512,3072,512,53/512,/0/default.jpg',
+      '/1024,0,512,512/512,/0/default.jpg',
+      '/1024,512,512,512/512,/0/default.jpg',
+      '/1024,1024,512,512/512,/0/default.jpg',
+      '/1024,1536,512,512/512,/0/default.jpg',
+      '/1024,2048,512,512/512,/0/default.jpg',
+      '/1024,2560,512,512/512,/0/default.jpg',
+      '/1024,3072,512,53/512,/0/default.jpg',
+      '/1536,0,384,512/384,/0/default.jpg',
+      '/1536,512,384,512/384,/0/default.jpg',
+      '/1536,1024,384,512/384,/0/default.jpg',
+      '/1536,1536,384,512/384,/0/default.jpg',
+      '/1536,2048,384,512/384,/0/default.jpg',
+      '/1536,2560,384,512/384,/0/default.jpg',
+      '/1536,3072,384,53/384,/0/default.jpg',
+      '/0,0,1024,1024/512,/0/default.jpg',
+      '/0,1024,1024,1024/512,/0/default.jpg',
+      '/0,2048,1024,1024/512,/0/default.jpg',
+      '/0,3072,1024,53/512,/0/default.jpg',
+      '/1024,0,896,1024/448,/0/default.jpg',
+      '/1024,1024,896,1024/448,/0/default.jpg',
+      '/1024,2048,896,1024/448,/0/default.jpg',
+      '/1024,3072,896,53/448,/0/default.jpg',
+      '/0,0,1920,2048/480,/0/default.jpg',
+      '/0,2048,1920,1077/480,/0/default.jpg',
+      '/full/240,/0/default.jpg',
+      '/full/120,/0/default.jpg',
+    ].map { |partial_path| File.join(output_dir, partial_path) }
+  end
+
+  describe "#for" do
+    it "should successfully generate the expected tile files" do
+      Imogen.with_image(fixture('sample.jpg').path) do |image|
+        Imogen::Iiif::Tiles.for(image, output_dir, :jpeg, tile_size) do |img, suggested_tile_dest_path, format, opts|
+          FileUtils.mkdir_p(File.dirname(suggested_tile_dest_path))
+          Imogen::Iiif.convert(img, suggested_tile_dest_path, format, opts)
+        end
+      end
+      generated_files = Dir["#{output_dir}/**/*.jpg"]
+
+      # Check for missing files and extra unexpected files
+      missing_files = expected_files.sort - generated_files.sort
+      extra_unexepcted_files = generated_files.sort - expected_files.sort
+      expect(missing_files).to eq([])
+      expect(extra_unexepcted_files).to eq([])
+    ensure
+      FileUtils.rm_rf(output_dir) if File.exist?(output_dir)
+    end
+  end
+
+  describe "#generate_vips_dzsave_tiles" do
+    pending "should successfully generate the expected tile files" do
+      Imogen.with_image(fixture('sample.jpg').path) do |image|
+        Imogen::Iiif::Tiles.generate_with_vips_dzsave(image, output_dir, format: 'jpg', tile_size: tile_size)
+      end
+      generated_files = Dir["#{output_dir}/**/*.jpg"]
+
+      # Check for missing files and extra unexpected files
+      missing_files = expected_files.sort - generated_files.sort
+      extra_unexepcted_files = generated_files.sort - expected_files.sort
+      # TODO: Remove this `if` statement and `warn` method call once the generate_vips_dzsave_tiles implementation works properly.
+      if missing_files.length.positive? || extra_unexepcted_files.length.positive?
+        warn "generate_with_vips_dzsave test:\nmissing_files: #{missing_files.length}, extra_unexepcted_files: #{extra_unexepcted_files.length}"
+      end
+      expect(missing_files).to eq([])
+      expect(extra_unexepcted_files).to eq([])
+    ensure
+      FileUtils.rm_rf(output_dir) if File.exist?(output_dir)
+    end
+  end
+end
+
--- spec/unit/imogen_iiif_tiles_spec.rb
+++ spec/unit/imogen_iiif_tiles_spec.rb
@@ -2,74 +2,92 @@ require 'imogen/iiif'
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Imogen::Iiif::Tiles, type: :unit do
   before(:all) do
-    @test_image = ImageStub.new(175,131)
+    @test_image = ImageStub.new(175, 131)
   end
-  let(:fullSize) { ["/0,0,175,131/full/0/native.jpg"]}
+  let(:tile512) {
+    [
+      '/full/175,/0/default.jpg'
+    ]
+  }
+  let(:tile256) {
+    [
+      '/full/175,/0/default.jpg',
+      '/full/88,/0/default.jpg'
+    ]
+  }
   let(:tile128) {
     [
-      "/0,0,128,128/full/0/native.jpg",
-      "/128,0,47,128/full/0/native.jpg",
-      "/0,128,128,3/full/0/native.jpg",
-      "/128,128,47,3/full/0/native.jpg"
+      '/0,0,128,128/128,/0/default.jpg',
+      '/0,128,128,3/128,/0/default.jpg',
+      '/128,0,47,128/47,/0/default.jpg',
+      '/128,128,47,3/47,/0/default.jpg',
+      '/full/88,/0/default.jpg',
+      '/full/44,/0/default.jpg'
     ]
   }
   let(:tile64) {
     [
-      "/0,0,64,64/full/0/native.jpg",
-      "/64,0,64,64/full/0/native.jpg",
-      "/128,0,47,64/full/0/native.jpg",
-      "/0,64,64,64/full/0/native.jpg",
-      "/64,64,64,64/full/0/native.jpg",
-      "/128,64,47,64/full/0/native.jpg",
-      "/0,128,64,3/full/0/native.jpg",
-      "/64,128,64,3/full/0/native.jpg",
-      "/128,128,47,3/full/0/native.jpg",
+      '/0,0,64,64/64,/0/default.jpg',
+      '/0,64,64,64/64,/0/default.jpg',
+      '/0,128,64,3/64,/0/default.jpg',
+      '/64,0,64,64/64,/0/default.jpg',
+      '/64,64,64,64/64,/0/default.jpg',
+      '/64,128,64,3/64,/0/default.jpg',
+      '/128,0,47,64/47,/0/default.jpg',
+      '/128,64,47,64/47,/0/default.jpg',
+      '/128,128,47,3/47,/0/default.jpg',
+      '/0,0,128,128/64,/0/default.jpg',
+      '/0,128,128,3/64,/0/default.jpg',
+      '/128,0,47,128/24,/0/default.jpg',
+      '/128,128,47,3/24,/0/default.jpg',
+      '/full/44,/0/default.jpg',
+      '/full/22,/0/default.jpg'
     ]
   }
-  describe '#scale_factor_for' do
-    it 'should calculate for different tile sizes' do
-      width, height = @test_image.width, @test_image.height
-      (0..8).each do |exp|
-        tile_size = 2**exp
-        expected = (8 - exp)**2
-        expect(subject.scale_factor_for(width, height, tile_size)).to eql(expected)
+  describe '#scale_factors_for' do
+    it 'should calculate the expected scale factors for the given image width, height, and tile_size' do
+      {
+        32 =>  [1, 2, 4, 8, 16],
+        64 =>  [1, 2, 4, 8],
+        128 => [1, 2, 4],
+        256 => [1, 2],
+        512 => [1]
+      }.each do |tile_size, expected_scale_factors|
+        expect(
+          described_class.scale_factors_for(@test_image.width, @test_image.height, tile_size)
+        ).to eq(expected_scale_factors)
       end
     end
   end
   describe '#for' do
-    it 'should produce a single tile when contained' do
-      expected = fullSize
+    it 'should produce the expected tiles for different tile sizes' do
+      {
+        512 => tile512,
+        256 => tile256,
+        128 => tile128,
+        64 => tile64
+      }.each do |tile_size, expected_tiles|
+        actual = []
+        described_class.for(@test_image, '', :jpeg, tile_size) do |img, dest_path, format, opts|
+          actual << dest_path
+        end
+        expect(actual).to eql(expected_tiles)
+      end
+    end
+    it 'should produce png when requested' do
+      expected = tile256.collect {|x| x.sub(/jpg$/, 'png')}
+      expected.uniq!
       actual = []
-      subject.for(@test_image,'',:jpeg,256) do |img,dest_path,format,opts|
+      described_class.for(@test_image, '', :png, 256) do |img, dest_path, format, opts|
         actual << dest_path
       end
       expect(actual).to eql(expected)
     end
-    it 'should produce a tileset when half' do
-      expected = fullSize + tile128
+    it 'should produce the expected quality when requested' do
+      expected = tile256.collect {|x| x.sub(/default.jpg$/, 'color.jpg')}
       expected.uniq!
       actual = []
-      subject.for(@test_image,'',:jpeg,128) do |img,dest_path,format,opts|
-        actual << dest_path
-      end
-      actual.uniq!
-      expect(actual.sort).to eql(expected.sort)
-    end
-    it 'should produce a single tileset' do
-      expected = fullSize + tile128 + tile64
-      expected.uniq!
-      actual = []
-      subject.for(@test_image,'',:jpeg,64) do |img,dest_path,format,opts|
-        actual << dest_path
-      end
-      actual.uniq!
-      expect(actual.sort).to eql(expected.sort)
-    end
-    it 'should produce png when requested' do
-      expected = fullSize.collect {|x| x.sub(/jpg$/,'png')}
-      expected.uniq!
-      actual = []
-      subject.for(@test_image,'',:png,256) do |img,dest_path,format,opts|
+      described_class.for(@test_image, '', :jpg, 256, 'color') do |img, dest_path, format, opts|
         actual << dest_path
       end
       expect(actual).to eql(expected)
